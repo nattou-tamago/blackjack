1e8f494358514ced4fc57ca80c0879323dd3a174
nattou-tamago <88647501+nattou-tamago@users.noreply.github.com>
2022-05-31 17:52:59 +0900

    enable user to bet

--- src/Evaluator.php
+++ src/Evaluator.php
@@ -5,12 +5,19 @@ namespace Blackjack;
 
 class Evaluator
 {
-    public function getWinner(array $results): void
+    private array $judgmentResult = [
+        'win' => false,
+        'lose' => false,
+        'push' => false,
+    ];
+
+    public function judge(array $results): array
     {
         if (count($results) === 1) {
             echo $results[0]['name'] . 'の得点が21を超えました。' . PHP_EOL;
             echo $results[0]['name'] . 'の負けです。。。' . PHP_EOL;
-            return;
+            $this->judgmentResult['lose'] = true;
+            return $this->judgmentResult;
         }
 
         foreach ($results as $result) {
@@ -19,10 +26,15 @@ class Evaluator
 
         if ($results[1]['bust'] || $results[0]['point'] > $results[1]['point']) {
             echo 'あなたの勝ちです！' . PHP_EOL;
+            $this->judgmentResult['win'] = true;
         } elseif ($results[0]['point'] < $results[1]['point']) {
             echo 'あなたの負けです。。。' . PHP_EOL;
+            $this->judgmentResult['lose'] = true;
         } else {
             echo '引き分けです。' . PHP_EOL;
+            $this->judgmentResult['push'] = true;
         }
+
+        return $this->judgmentResult;
     }
 }
